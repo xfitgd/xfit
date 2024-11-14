2794a1ae80a9fd3f989c890ccc89deb083b91428
Kim Seong Jun <xfitgd@gmail.com>
2024-11-14 20:16:06 +0900

    linux fix

--- engine/__linux.zig
+++ engine/__linux.zig
@@ -322,8 +322,8 @@ pub fn linux_start() void {
 
     if (__system.init_set.window_width == xfit.init_setting.DEF_SIZE or __system.init_set.window_width == 0) __system.init_set.window_width = @as(u32, @intCast(__system.monitors.items[__system.init_set.screen_index].rect.width())) / 2;
     if (__system.init_set.window_height == xfit.init_setting.DEF_SIZE or __system.init_set.window_height == 0) __system.init_set.window_height = @as(u32, @intCast(__system.monitors.items[__system.init_set.screen_index].rect.height())) / 2;
-    if (__system.init_set.window_x == xfit.init_setting.DEF_POS) __system.init_set.window_x = 0;
-    if (__system.init_set.window_y == xfit.init_setting.DEF_POS) __system.init_set.window_y = 0;
+    if (__system.init_set.window_x == xfit.init_setting.DEF_POS) __system.init_set.window_x = __system.monitors.items[__system.init_set.screen_index].rect.left + @divFloor(__system.monitors.items[__system.init_set.screen_index].rect.width(), 4);
+    if (__system.init_set.window_y == xfit.init_setting.DEF_POS) __system.init_set.window_y = __system.monitors.items[__system.init_set.screen_index].rect.top + @divFloor(__system.monitors.items[__system.init_set.screen_index].rect.height(), 4);
 
     wnd = c.XCreateWindow(
         display,
